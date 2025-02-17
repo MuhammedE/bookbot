10d8eef4412441339b32e8893c8156dfd2a6fd30
MuhammedE <muhammed.elma@gmail.com>
2025-02-18 00:41:53 +0100

    added print_report function

--- main.py
+++ main.py
@@ -2,6 +2,46 @@ def count_words(text):
     words = text.split()
     return len(words)
 
+def count_characters(text):
+    character_counts = {}
+    
+    text_lowercase = text.lower()
+
+    for c in text_lowercase:
+        if c in character_counts:
+            character_counts[c] += 1
+        else:
+            character_counts[c] = 1
+    return character_counts
+
+def print_report():
+    with open("books/frankenstein.txt") as f:
+        text = f.read()
+    
+    counts_dict = count_characters(text)
+    word_count = count_words(text)
+
+    list_counts_dict = []
+
+    for k in counts_dict:
+        dict_item = {}
+        if k.isalpha():
+            dict_item["char"] = k
+            dict_item["count"] = counts_dict[k]
+            list_counts_dict.append(dict_item)
+    list_counts_dict.sort(reverse=True,key=sort_on)
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{word_count} words found in the document")
+    for dict_item in list_counts_dict:
+        print(f"The '{dict_item["char"]}' character was found {dict_item["count"]} times")
+    print("--- End report ---")
+    
+
+
+def sort_on(dict):
+    return dict["count"]
+
 
 
 
@@ -9,9 +49,11 @@ def count_words(text):
 
 
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-    print(count_words(file_contents))
+    print_report()
+    
+    
+
+
 
 
 main()
